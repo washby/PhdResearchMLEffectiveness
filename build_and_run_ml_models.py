3e01b8a6bfb34b1adff37fde2f55d74a8a105d8f
Wade Ashby <wade.ashby@hotmail.com>
2023-10-08 08:14:32 -0500

    Updated log methodology and set n to 40,000 for next run.

--- build_and_run_ml_models.py
+++ build_and_run_ml_models.py
@@ -10,8 +10,9 @@ import datetime
 
 if __name__ == '__main__':
 
+    date_str = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                        filename=fr'logs\ml_{datetime.datetime.now().replace(":", "-")}.log', filemode='w')
+                        filename=fr'logs\ml_{date_str}.log', filemode='w')
 
     header_list = ["user_id", "course_id", "total_activity_time", "user_state", "term_id", "crs_count", "avg_crs_level",
                    "avg_crs_credits", "grade", "adjusted_grade", "submission_day_diff", "pg_level", "pg_normed",
